0c18b91e2f33102e56fc85a745b82a97d62da3e8
KaikyRMendes <107228751+KaikyRMendes@users.noreply.github.com>
2023-04-03 00:25:25 -0300

    Add files via upload

---
+++ TCC/js/sidebar.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    const menuMobile = document.getElementById("menu-mobile")
+    if(menuMobile.className === "menu-mobile-active"){
+        menuMobile.className = "menu-mobile"
+    } else{
+        menuMobile.className = "menu-mobile-active"
+    }
+}
