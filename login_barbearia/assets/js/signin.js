efebcb5fde44db84c937f60a550680fa34f0a556
JhonDSM <100415194+JhonDSM@users.noreply.github.com>
2023-04-13 19:01:13 -0300

    Add files via upload

--- login_barbearia/assets/js/signin.js
+++ login_barbearia/assets/js/signin.js
@@ -1,61 +1,73 @@
-let btn = document.querySelector('.fa-eye')
+let btn = document.querySelector('.fa-eye');
 
 btn.addEventListener('click', ()=>{
-  let inputSenha = document.querySelector('#senha')
+  let inputSenha = document.querySelector('#senha');
   
   if(inputSenha.getAttribute('type') == 'password'){
-    inputSenha.setAttribute('type', 'text')
+    inputSenha.setAttribute('type', 'text');
   } else {
-    inputSenha.setAttribute('type', 'password')
+    inputSenha.setAttribute('type', 'password');
   }
-})
+});
 
 function entrar(){
-  let usuario = document.querySelector('#usuario')
-  let userLabel = document.querySelector('#userLabel')
+  let usuario = document.querySelector('#usuario');
+  let userLabel = document.querySelector('#userLabel');
   
-  let senha = document.querySelector('#senha')
-  let senhaLabel = document.querySelector('#senhaLabel')
+  let senha = document.querySelector('#senha');
+  let senhaLabel = document.querySelector('#senhaLabel');
   
-  let msgError = document.querySelector('#msgError')
-  let listaUser = []
+  let msgError = document.querySelector('#msgError');
+  let listaUser = [];
   
   let userValid = {
     nome: '',
     user: '',
-    senha: ''
-  }
+    senha: '',
+    nivelAcesso: ''
+  };
   
-  listaUser = JSON.parse(localStorage.getItem('listaUser'))
+  listaUser = JSON.parse(localStorage.getItem('listaUser'));
   
   listaUser.forEach((item) => {
     if(usuario.value == item.userCad && senha.value == item.senhaCad){
-       
       userValid = {
          nome: item.nomeCad,
          user: item.userCad,
-         senha: item.senhaCad
-       }
-      
+         senha: item.senhaCad,
+         nivelAcesso: item.nivelAcessoCad
+      };
     }
-  })
+  });
    
   if(usuario.value == userValid.user && senha.value == userValid.senha){
-    window.location.href = '../../index.html'
+    let mathRandom = Math.random().toString(16).substr(2);
+    let token = mathRandom + mathRandom;
     
-    let mathRandom = Math.random().toString(16).substr(2)
-    let token = mathRandom + mathRandom
+    localStorage.setItem('token', token);
+    localStorage.setItem('userLogado', JSON.stringify(userValid));
     
-    localStorage.setItem('token', token)
-    localStorage.setItem('userLogado', JSON.stringify(userValid))
+    verificarNivelAcesso(userValid.nivelAcesso);
   } else {
-    userLabel.setAttribute('style', 'color: red')
-    usuario.setAttribute('style', 'border-color: red')
-    senhaLabel.setAttribute('style', 'color: red')
-    senha.setAttribute('style', 'border-color: red')
-    msgError.setAttribute('style', 'display: block')
-    msgError.innerHTML = 'Usuário ou senha incorretos'
-    usuario.focus()
+    userLabel.setAttribute('style', 'color: red');
+    usuario.setAttribute('style', 'border-color: red');
+    senhaLabel.setAttribute('style', 'color: red');
+    senha.setAttribute('style', 'border-color: red');
+    msgError.setAttribute('style', 'display: block');
+    msgError.innerHTML = 'Usuário ou senha incorretos';
+    usuario.focus();
   }
-  
+}
+
+function verificarNivelAcesso(nivelAcesso) {
+  switch(nivelAcesso) {
+    case 'admin':
+      window.location.href = 'admin.html';
+      break;
+    case 'user':
+      window.location.href = 'user.html';
+      break;
+    default:
+      alert('Nível de acesso inválido');
+  }
 }
