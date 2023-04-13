033eea3485fb2cf67c16b955645a8e389ff646d8
JhonDSM <100415194+JhonDSM@users.noreply.github.com>
2023-04-13 19:25:40 -0300

    Update index.js

--- login_barbearia/assets/js/index.js
+++ login_barbearia/assets/js/index.js
@@ -5,12 +5,15 @@ if (localStorage.getItem("token") == null) {
 
 const userLogado = JSON.parse(localStorage.getItem("userLogado"));
 
-if (userLogado.nivelAcesso != "1") {
-  window.location.href = "";
-
-if (userLogado.nivelAcesso != "2") {
-  window.location.href = "";
-  
+if (userLogado.nivelAcesso != "1" && userLogado.nivelAcesso != "2") {
+  alert("Você não tem permissão para acessar essa página");
+  window.location.href = "../html/index.html";
+} else if (userLogado.nivelAcesso == "1") {
+  // redirecionar para a página de administração
+  window.location.href = "../html/admin.html";
+} else if (userLogado.nivelAcesso == "2") {
+  // redirecionar para a página de master
+  window.location.href = "../html/master.html";
 }
 
 const logado = document.querySelector("#logado");
