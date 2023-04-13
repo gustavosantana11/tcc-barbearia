91763691c30c2aa15dd0a2e4232296b4dfa6abe1
JhonDSM <100415194+JhonDSM@users.noreply.github.com>
2023-04-13 19:07:54 -0300

    Update index.js

--- login_barbearia/assets/js/index.js
+++ login_barbearia/assets/js/index.js
@@ -1,15 +1,20 @@
 if (localStorage.getItem("token") == null) {
-    alert("Você precisa estar logado para acessar essa página");
-    window.location.href = "./assets/html/signin.html";
-  }
-  
-  const userLogado = JSON.parse(localStorage.getItem("userLogado"));
-  
-  const logado = document.querySelector("#logado");
-  logado.innerHTML = `Olá ${userLogado.nome}`;
-  
-  function sair() {
-    localStorage.removeItem("token");
-    localStorage.removeItem("userLogado");
-    window.location.href = "./assets/html/signin.html";
-  }
+  alert("Você precisa estar logado para acessar essa página");
+  window.location.href = "../html/signin.html";
+}
+
+const userLogado = JSON.parse(localStorage.getItem("userLogado"));
+
+if (userLogado.nivelAcesso != "admin") {
+  alert("Você não tem permissão para acessar essa página");
+  window.location.href = "../html/index.html";
+}
+
+const logado = document.querySelector("#logado");
+logado.innerHTML = `Olá ${userLogado.nome}`;
+
+function sair() {
+  localStorage.removeItem("token");
+  localStorage.removeItem("userLogado");
+  window.location.href = "../html/signin.html";
+}
