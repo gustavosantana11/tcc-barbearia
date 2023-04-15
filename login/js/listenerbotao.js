0487e4860cf31cb991741a5cc7f440ed1f97781f
Caneta Azul <120990028+gustavosantana11@users.noreply.github.com>
2023-04-15 15:31:04 -0300

    Add files via upload

---
+++ login/js/listenerbotao.js
@@ -0,0 +1,17 @@
+// Event listener para o botão de login
+document.querySelector('#btnLogin').addEventListener('click', () => {
+  const nome = document.querySelector('#txtUsuario').value;
+  const senha = document.querySelector('#txtSenha').value;
+
+  const usuario = Autenticacao.autenticarUsuario(nome, senha);
+
+  if (usuario != null) {
+    if (usuario.nivelAcesso === 1) {
+      // Redirecionar para a tela de ADM
+    } else if (usuario.nivelAcesso === 2) {
+      // Redirecionar para a tela de MASTER
+    }
+  } else {
+    alert('Usuário ou senha inválidos');
+  }
+});
---
+++ login/js/usuario.js
@@ -0,0 +1,8 @@
+// Classe Usuario
+class Usuario {
+  constructor(nome, senha, nivelAcesso) {
+    this.nome = nome;
+    this.senha = senha;
+    this.nivelAcesso = nivelAcesso;
+  }
+}
