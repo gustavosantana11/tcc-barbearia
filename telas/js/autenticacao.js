d97b2356b29614e669672a8b686179caf7410198
Caneta Azul <120990028+gustavosantana11@users.noreply.github.com>
2023-04-15 15:30:26 -0300

    Add files via upload

---
+++ telas/js/autenticacao.js
@@ -0,0 +1,17 @@
+// Classe Autenticacao
+class Autenticacao {
+    static usuarios = [
+      new Usuario('aquilesbarbeiro', '12345', 1),
+      new Usuario('kaikyadm', '54321', 2)
+    ];
+  
+    static autenticarUsuario(nome, senha) {
+      for (let i = 0; i < this.usuarios.length; i++) {
+        const usuario = this.usuarios[i];
+        if (usuario.nome === nome && usuario.senha === senha) {
+          return usuario;
+        }
+      }
+      return null;
+    }
+  }
