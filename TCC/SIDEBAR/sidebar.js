16c13965e7373d84270a477783047631129ce7f5
Lucas <126921719+LN05@users.noreply.github.com>
2023-06-02 08:49:42 -0300

    Delete TCC directory

--- TCC/SIDEBAR/sidebar.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu(){
-    const menuMobile = document.getElementById("menu-mobile")
-    if(menuMobile.className === "menu-mobile-active"){
-        menuMobile.className = "menu-mobile"
-    } else{
-        menuMobile.className = "menu-mobile-active"
-    }
-}
